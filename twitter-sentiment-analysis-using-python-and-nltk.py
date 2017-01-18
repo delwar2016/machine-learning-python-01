6a663dd817bb08ae93c4aa263c2ca0b0068d1135
Md. Delwar Hossain <twinkle_023020@yahoo.com>
2017-01-19 01:07:36 +0600

    Update readme and complete machine learning example

---
+++ twitter-sentiment-analysis-using-python-and-nltk.py
@@ -0,0 +1,65 @@
+#The following list contains the positive tweets:
+pos_tweets = [('I love this car', 'positive'),
+              ('This view is amazing', 'positive'),
+              ('I feel great this morning', 'positive'),
+              ('I am so excited about the concert', 'positive'),
+              ('He is my best friend', 'positive')]
+
+#The following list contains the negative tweets:
+neg_tweets = [('I do not like this car', 'negative'),
+              ('This view is horrible', 'negative'),
+              ('I feel tired this morning', 'negative'),
+              ('I am not looking forward to the concert', 'negative'),
+              ('He is my enemy', 'negative')]
+
+# We take both of those lists and create a single list of tuples each containing two elements.
+# First element is an array containing the words and second element is the type of sentiment. We get rid of the words smaller than 2 characters and we use lowercase for everything.
+
+
+tweets = []
+for (words, sentiment) in pos_tweets + neg_tweets:
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+    tweets.append((words_filtered, sentiment))
+
+
+test_tweets = [
+    (['feel', 'happy', 'this', 'morning'], 'positive'),
+    (['larry', 'friend'], 'positive'),
+    (['not', 'like', 'that', 'man'], 'negative'),
+    (['house', 'not', 'great'], 'negative'),
+    (['your', 'song', 'annoying'], 'negative')]
+
+import nltk
+from operator import itemgetter
+
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+      all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    wordlist_sorted = sorted(wordlist.items(), key=lambda x: x[1], reverse=True);
+    word_features = map(itemgetter(0), wordlist_sorted)
+    return word_features
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+training_set = nltk.classify.apply_features(extract_features, tweets)
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+tweet = 'The movie is not bad'
+print classifier.classify(extract_features(tweet.split()))
+print extract_features(tweet.split())
+
+
+
